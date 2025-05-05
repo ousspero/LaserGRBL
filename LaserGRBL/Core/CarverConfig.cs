b8e1f8dd251b375e271ec1a42e32c29b7a109c40
Oussama B <ousspero@gmail.com>
2025-05-05 21:11:59 +0200

    Send finished order to PC

---
+++ LaserGRBL/Core/CarverConfig.cs
@@ -0,0 +1,15 @@
+﻿//Copyright (c) 2016-2021 Diego Settimi - https://github.com/arkypita/
+
+using System.Net;
+
+namespace LaserGRBL
+{
+    public class CarverConfig
+    {
+        public string PcIP { get; set; } = "172.31.1.130";
+        public int PcPort { get; set; } = 30000;
+        public CarverConfig()
+        {
+        }
+    }
+}
